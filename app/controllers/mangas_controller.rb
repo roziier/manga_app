f3d58674df50d0a8da4e1407f0ff9056bb30a1fc
roziier <71633856+roziier@users.noreply.github.com>
2024-04-01 16:28:56 +0100

    Add create and new + CSS
    Add create and new + CSS

--- app/controllers/mangas_controller.rb
+++ app/controllers/mangas_controller.rb
@@ -2,4 +2,29 @@ class MangasController < ApplicationController
     def index
         @mangas = Manga.all
     end
+
+    def show
+        @manga = Manga.find(params[:id])
+    end 
+
+    def new
+        @manga = Manga.new
+    end
+
+    def create
+        @manga = Manga.new(params.require(:manga).permit(:title, :mangaka, :collected, :status))
+        if @manga.save
+            flash[:notice] = "New manga was created successfully"
+            redirect_to @manga
+        else
+            render 'new'
+        end
+    end
+
+    def destroy
+        @manga = Manga.find(params[:id])
+        @manga.destroy
+        flash[:notice] = "Manga was deleted successfully"
+        redirect_to mangas_path
+    end
 end
