174e2333a52686b6ca245b8b8d4777b55ba45689
roziier <71633856+roziier@users.noreply.github.com>
2024-04-02 09:00:26 +0100

    Added edit and update
    Added edit and update - CRUD completed

--- app/controllers/mangas_controller.rb
+++ app/controllers/mangas_controller.rb
@@ -8,16 +8,30 @@ class MangasController < ApplicationController
     end 
 
     def new
-        @manga = Manga.new
+        @manga = Manga.new()
     end
 
     def create
         @manga = Manga.new(params.require(:manga).permit(:title, :mangaka, :collected, :status))
         if @manga.save
             flash[:notice] = "New manga was created successfully"
-            redirect_to @manga
+            redirect_to manga_path(@manga)
         else
             render 'new'
+        end
+    end
+
+    def edit
+        @manga = Manga.find(params[:id])
+    end
+
+    def update
+        @manga = Manga.find(params[:id])
+        if @manga.update(params.require(:manga).permit(:title, :mangaka, :collected, :status))
+            flash[:notice] = "Your manga was updated successfully"
+            redirect_to manga_path(@manga)
+        else
+            render 'edit'
         end
     end
 
